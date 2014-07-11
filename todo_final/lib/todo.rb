56bb3f7f9519cf8cbceef041b1cf65c56d96f189
jagadeeshrampati <jagadeeshrampati@railsfactory.org>
2014-07-11 12:05:43 +0530

    satisfying all test cases

--- todo_final/lib/todo.rb
+++ todo_final/lib/todo.rb
@@ -18,11 +18,14 @@ return @todo.size
 end
 
 def self.completed
-return @completed.size
+return @completed.length
 end
 
 def self.add(items)
+@todo << items
 @pending << items
+#self.save
+#self.load1
 return @pending.count
 end
 
@@ -34,27 +37,49 @@ end
 
 def self.delete(num)
 @completed.delete_at(num-1)
+#self.save
+#self.load1
 return @completed.size
 end
 
 def self.modify(num , item)
 @pending[num-1] = item
-return @pending[num-1]
+#self.save
+#self.load1
+return @pending.length
 end
 
 def self.empty
 @pending.clear
 @completed.clear
 @todo.clear
+#self.save
+#self.load1
 return true
 end
 
 def self.show_pending(num)
+#self.save
+#self.load1
 return @pending[num-1]
 end
 
 def self.show_completed(num)
+#self.save
+#self.load1
 return @completed[num-1]
 end
-
+def self.save
+f = File.open("todo.txt", "w")
+str =""
+str = @todo.join("\n")
+f.write(str)
+f.close
+return true
+end
+def self.load1
+@todo = open('todo.txt').map { |line| line.split('\n')[0] }
+@completed = @todo.select { |c| c.match(/#Done/) }
+@pending = @todo - @completed
+end
 end
