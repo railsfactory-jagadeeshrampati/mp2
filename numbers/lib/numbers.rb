a3f8da0a77ed5b8f12ece998b0cf4286cd2e7136
jagadeeshrampati <jagadeeshrampati@railsfactory.org>
2014-07-15 18:51:45 +0530

    convert numbers to date format like 1 to 1st

--- numbers/lib/numbers.rb
+++ numbers/lib/numbers.rb
@@ -1,20 +1,18 @@
 class Numbers
 
   def self.convert(number)
-      num = number
-        if num%10 == 1
-           "#{number}st"
-        elsif num%10 == 2
-           "#{number}nd"
-         elsif num <20 and num>10
+      num = number.to_i
+        if num <20 and num>10
              "#{number}th"
+         elsif  num%10 == 1
+           "#{number}st"
+          elsif num%10 == 2
+           "#{number}nd"
           elsif num%10 == 3
             "#{number}rd"
          else
-                 "#{number}th"
+                 "#{number.to_i}th"
      end
                              
 end 
 end 
-
-
